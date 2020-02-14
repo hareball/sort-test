0c6133172a21aced2ac93fc76c19dadaf6f2cf5e
Dan <haredd@gmail.com>
2020-02-14 14:47:01 +1100

    Add files via upload

--- rtest.c
+++ rtest.c
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARRAYSIZE 	50000
+#define ARRAYSIZE 		50000
+#define QS_MAX_LEVELS  		1000
+
 
 /* Predeclare functions */
 void do_displaysortedlist(int *sortedlist);
-void do_bubblesort(int *sortlist);
-void do_insertionsort(int *sortlist);
-void do_quicksort(int *sortlist, int elements);
+void do_bubblesort( int *sortlist );
+void do_insertionsort( int *sortlist );
+void do_quicksort( int *sortlist );
 
 int main()
 {
 	int msec;
 	time_t t;
-	int randnums[ARRAYSIZE], bsrandnums[ARRAYSIZE], israndnums[ARRAYSIZE], qsrandnums[ARRAYSIZE];
+	int randnums[ ARRAYSIZE ], bsrandnums[ ARRAYSIZE ],
+	    israndnums[ ARRAYSIZE ], qsrandnums[ ARRAYSIZE ];
 	clock_t start, diff;
 
-	/* Init random number generator */
-	srand((unsigned) time(&t));
+	// Init random number generator
+	srand( (unsigned) time( &t ) );
 
-	for (int x = 0; x < ARRAYSIZE; x++)
+	for ( int x = 0; x < ARRAYSIZE; x++ )
 	{
 		randnums[x] = rand();
 		bsrandnums[x] = randnums[x];
@@ -28,63 +31,59 @@ int main()
 		qsrandnums[x] = randnums[x];
 	}
 
-	printf("*** %d Random numbers generated.\n\r", ARRAYSIZE);
+	printf( "*** %d Random numbers generated.\n", ARRAYSIZE );
 
-
-	printf("*** Bubble Sort starting... ");
-	fflush(stdout);
+	printf( "*** Bubble Sort starting... " );
+	fflush( stdout );
 
 	start = clock();
-	do_bubblesort(bsrandnums);
+	do_bubblesort( bsrandnums );
 	diff = clock() - start;
 
 	msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("complete. Time taken:[%d.%03d] seconds.\n\r", msec/1000, msec%1000);
-	do_displaysortedlist(bsrandnums);
-	
+	printf( "complete. Time taken:[%d.%03d] seconds.\n", msec/1000, msec%1000 );
+	do_displaysortedlist( bsrandnums );
 
-	printf("\n\r*** Insertion Sort starting... ");
-	fflush(stdout);
+	printf( "\n*** Insertion Sort starting... " );
+	fflush( stdout );
 
 	start = clock();
-	do_insertionsort(israndnums);
+	do_insertionsort( israndnums );
 	diff = clock() - start;
-	
+
 	msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("complete. Time taken:[%d.%03d] seconds.\n\r", msec/1000, msec%1000);
-	do_displaysortedlist(israndnums);
+	printf( "complete. Time taken:[%d.%03d] seconds.\n", msec/1000, msec%1000 );
+	do_displaysortedlist( israndnums );
 
-
-
-	printf("\n\r*** Quick Sort starting... ");
-	fflush(stdout);
+	printf( "\n*** Quick Sort starting... " );
+	fflush( stdout );
 
 	start = clock();
-	do_quicksort(qsrandnums, ARRAYSIZE);
+	do_quicksort( qsrandnums );
 	diff = clock() - start;
-	
-	msec = (diff * 1000.0) / CLOCKS_PER_SEC;
-	printf("complete. Time taken:[%d.%03d] seconds.\n\r", msec/1000, msec%1000);
-	do_displaysortedlist(qsrandnums);
 
-	return (0);
+	msec = ( diff * 1000.0 ) / CLOCKS_PER_SEC;
+	printf( "complete. Time taken:[%d.%03d] seconds.\n", msec/1000, msec%1000 );
+	do_displaysortedlist( qsrandnums );
+
+	return( 0 );
 }
 
-void do_displaysortedlist(int *sortedlist)
+void do_displaysortedlist( int *sortedlist )
 {
-	for (int x = 0; x < ARRAYSIZE; x++)
+	for( int x = 0; x < ARRAYSIZE; x++ )
 	{
-		if( x < 4 || x > (ARRAYSIZE - 5))
-			printf("[%d] - %d\n\r", x, sortedlist[x]);
-		
+		if( x < 4 || x > ( ARRAYSIZE - 5 ) )
+			printf( "[%d] - %d\n", x, sortedlist[ x ] );
+
 		if( x == 5 )
-			printf("....\n\r");
+			printf( "....\n" );
 	}
 
 	return;
 }
 
-void do_bubblesort(int *sortlist)
+void do_bubblesort( int *sortlist )
 {
 	int x = 0, tmpSwap = 0;
 	long int iter = 0;
@@ -92,116 +91,119 @@ void do_bubblesort(int *sortlist)
 	do
 	{
 		tmpSwap = 0;
-		
-		for( x=0; x<(ARRAYSIZE-1); x++ )
+
+		for( x = 0; x < ( ARRAYSIZE - 1 ); x++ )
 		{
-			if( sortlist[x] > sortlist[x+1])
+			if( sortlist[ x ] > sortlist[ x + 1 ] )
 			{
 				//swap
-				tmpSwap = sortlist[x+1];
-				sortlist[x+1] = sortlist[x];
-				sortlist[x] = tmpSwap;
+				tmpSwap = sortlist[ x + 1 ];
+				sortlist[ x + 1 ] = sortlist[ x ];
+				sortlist[ x ] = tmpSwap;
 			}
 			iter++;
 		}
-		
-	} while (tmpSwap);
 
-	printf("[%ld iterations]... ", iter);
-	
+	} while( tmpSwap );
+
+	printf( "[%ld iterations]... ", iter );
+
 	return;
 }
 
-void do_insertionsort(int *sortlist)
+void do_insertionsort( int *sortlist )
 {
 	int x = 0, tmpSwap = 0;
 	long int iter = 0;
-	
-	for( x=1; x<(ARRAYSIZE); x++ )
+
+	for( x = 1; x < ( ARRAYSIZE ); x++ )
 	{
 		int cpos = x;
-		
+
 		// take it to the top
 		do
-		{	
-			if( sortlist[cpos] < sortlist[cpos-1] )
+		{
+			if( sortlist[ cpos ] < sortlist[ cpos - 1 ] )
 			{
 				//swap them
-				tmpSwap = sortlist[cpos-1];
-				sortlist[cpos-1] = sortlist[cpos];
-				sortlist[cpos] = tmpSwap;
+				tmpSwap = sortlist[ cpos - 1 ];
+				sortlist[ cpos - 1 ] = sortlist[ cpos ];
+				sortlist[ cpos ] = tmpSwap;
 			}
-			
+
 			cpos--;
-			
+
 			iter++;
-			
-		} while (sortlist[cpos] < sortlist[cpos-1] &&
-				cpos > 0 );
+
+		} while( sortlist[ cpos ] < sortlist[ cpos - 1 ] &&
+			 cpos > 0 );
 	}
-	
-	printf("[%ld iterations]... ", iter);
-	
+
+	printf( "[%ld iterations]... ", iter );
+
 	return;
 }
 
 
-void do_quicksort(int *sortlist, int elements)
+void do_quicksort( int *sortlist )
 {
-
-	#define  MAX_LEVELS  1000
-
-	int  piv, beg[MAX_LEVELS], end[MAX_LEVELS], i=0, L, R;
+	int  piv, beg[ QS_MAX_LEVELS ], end[ QS_MAX_LEVELS ], i = 0, L, R;
 
 	long int iter = 0;
 
-	beg[0]=0; end[0]=elements;
+	beg[ 0 ] = 0;
+	end[ 0 ] = ARRAYSIZE;
 
-	while (i>=0) 
+	while( i >= 0 )
 	{
-		L=beg[i]; R=end[i]-1;
-		if (L<R) 
+		L = beg[ i ];
+		R = end[ i ] - 1;
+		if( L < R )
 		{
-			piv=sortlist[L]; 
-			if (i==MAX_LEVELS-1) 
+			piv = sortlist[ L ];
+			if( i == QS_MAX_LEVELS - 1 )
 			{
 				return;
 			}
 
-		  	while (L<R) 
+		  	while( L < R )
 			{
-				while (sortlist[R]>=piv && L<R) 
-				{				
+				while( sortlist[ R ] >= piv && L < R )
+				{
 					R--;
 					iter++;
 				}
-				if (L<R)
+
+				if( L < R )
 				{
-					sortlist[L++]=sortlist[R];
+					sortlist[ L++ ] = sortlist[ R ];
 				}
-				while (sortlist[L]<=piv && L<R) 
+
+				while( sortlist[ L ] <= piv && L < R )
 				{
 					L++;
 					iter++;
 				}
-				if (L<R) 
+
+				if( L < R )
 				{
-					sortlist[R--]=sortlist[L];
+					sortlist[ R-- ] = sortlist[ L ];
 				}
 			}
-		  	sortlist[L]=piv; 
-			beg[i+1]=L+1; 
-			end[i+1]=end[i]; 
-			end[i++]=L; 
+
+		  	sortlist[ L ] = piv;
+			beg[ i + 1 ] = L + 1;
+			end[ i + 1 ] = end[ i ];
+			end[ i++ ] = L;
 		}
-		else 
+		else
 		{
 			i--;
 			iter++;
 		}
 	}
 
-	printf("[%ld iterations]... ", iter);
+	printf( "[%ld iterations]... ", iter );
 
-	return; 
+	return;
 }
